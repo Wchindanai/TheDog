6375fe6ff9e6814dfbac275d00fadb097878be8e
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-07-10 09:40:57 +0700

    add accident activitu

---
+++ app/src/main/java/com/example/dream/thedog/AccidentDetail.java
@@ -0,0 +1,13 @@
+package com.example.dream.thedog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccidentDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accident_detail);
+    }
+}
